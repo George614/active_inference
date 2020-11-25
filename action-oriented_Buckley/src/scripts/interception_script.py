4e9cea0194f16cc78213885bc898db1b4f74b105
George <yangzhizhuo@gmail.com>
2020-11-25 16:58:01 -0500

    Added parallel computation for training agents

--- action-oriented_Buckley/src/scripts/interception_script.py
+++ action-oriented_Buckley/src/scripts/interception_script.py
@@ -9,82 +9,85 @@ is learned and mutilple agents learned separately are saved.
 import numpy as np
 import sys
 import os
-import animate_energy_plots as apt
+
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
 
 sys.path.append(os.getcwd() + '/..')
 import core
+import animate_energy_plots as apt
 from core.config import *
 
-TRAIN_STEPS = 5000
+TRAIN_STEPS = 1000
 TEST_STEPS = 0
-N_AGENTS = 100
+N_AGENTS = 4
 
-run_time = []  # runtime for full agents across trials
-episode_steps_full = []  # steps used per episode for all full agents
-episode_steps_inst = []  # steps used per episode for all full agents
-episode_steps_epis = []  # steps used per episode for all full agents
-full_records = []
-inst_records = []
-epis_records = []
-rand_records = []
+
+## handle path and create folder if necessary
+pathlist = os.getcwd().split(os.sep)
+path = os.path.join(pathlist[0], os.sep, *pathlist[1:-1], "data", CHANGE_DICT[OBV_OPTION])
+if CONTINUAL_LEARNING and TEST_STEPS<=0:
+    path = path + "_continual_learning_circle"
+elif CONTINUAL_LEARNING and TEST_STEPS>0:
+    path = path + "_shut_learning"
+if not os.path.isdir(path):
+    os.makedirs(path)
+    print("Created folder: ", path)
+
+
+def run_exp_parallel(n):
+    ''' wrapper function to train multiple agents of various types in parallel '''
+    print("> Processing agent #{}.".format(n))
+    # initialize agents
+    full_agent = core.get_mdp(FULL_ID)
+    inst_agent = core.get_mdp(INST_ID)
+    epis_agent = core.get_mdp(EPIS_ID)
+    rand_agent = core.get_mdp(RAND_ID)
+    
+    # learn each type of single agent individually 
+    full_agent, full_record = core.learn_record_trial(full_agent, TRAIN_STEPS, TEST_STEPS)
+    inst_agent, inst_record = core.learn_record_trial(inst_agent, TRAIN_STEPS, TEST_STEPS)
+    epis_agent, epis_record = core.learn_record_trial(epis_agent, TRAIN_STEPS, TEST_STEPS)
+    rand_agent, rand_record = core.learn_record_trial(rand_agent, TRAIN_STEPS, TEST_STEPS)
+    
+    ## generate visualizations and save as pdf and mp4 ##
+    # apt.plot_energy(full_record, n, path)
+    # apt.animate_energy_plots(full_record, n, path)
+    # apt.animate_trajectory(full_record, n, path)
+    
+    return full_record, inst_record, epis_record, rand_record
+    
 
 if __name__ == "__main__":
     print("Starting experiments with {} as observation, training {} agents...".format(CHANGE_DICT[OBV_OPTION], N_AGENTS))
-    pathlist = os.getcwd().split(os.sep)
-    path = os.path.join(pathlist[0], os.sep, *pathlist[1:-1], "data", CHANGE_DICT[OBV_OPTION])
-    if CONTINUAL_LEARNING and TEST_STEPS<=0:
-        path = path + "_continual_learning_circle"
-    elif CONTINUAL_LEARNING and TEST_STEPS>0:
-        path = path + "_shut_learning"
-    if not os.path.isdir(path):
-        os.makedirs(path)
-        print("Created folder: ", path)
-    for n in range(N_AGENTS):
-        # initialize agents
-        full_agent = core.get_mdp(FULL_ID)
-        inst_agent = core.get_mdp(INST_ID)
-        epis_agent = core.get_mdp(EPIS_ID)
-        rand_agent = core.get_mdp(RAND_ID)
-        
-        # learn each type of single agent individually 
-        full_agent, full_record = core.learn_record_trial(full_agent, TRAIN_STEPS, TEST_STEPS)
-        inst_agent, inst_record = core.learn_record_trial(inst_agent, TRAIN_STEPS, TEST_STEPS)
-        epis_agent, epis_record = core.learn_record_trial(epis_agent, TRAIN_STEPS, TEST_STEPS)
-        rand_agent, rand_record = core.learn_record_trial(rand_agent, TRAIN_STEPS, TEST_STEPS)
-        
-        # save record of trial information for all agents
-        full_records.append(full_record)
-        inst_records.append(inst_record)
-        epis_records.append(epis_record)
-        rand_records.append(rand_record)
-        # np.save(os.path.join(path, "full_single_record_{}.npy".format(n)), full_record)
-        # np.save(os.path.join(path, "inst_single_record_{}.npy".format(n)), inst_record)
-        # np.save(os.path.join(path, "epis_single_record_{}.npy".format(n)), epis_record)
-        # np.save(os.path.join(path, "rand_single_record_{}.npy".format(n)), rand_record)
-        # print("> Data saved for agent #{}.".format(n))
-          
-        # generate visualizations and save as pdf and mp4
-        # apt.plot_energy(full_record, n, path)
-        # apt.animate_energy_plots(full_record, n, path)
-        # apt.animate_trajectory(full_record, n, path)
-        
-        run_time.append(full_record["runtime"])
-        episode_steps_full.append(full_record["steps_episode"])
-        episode_steps_inst.append(inst_record["steps_episode"])
-        episode_steps_epis.append(epis_record["steps_episode"])
-        print("> Steps used per episode for EFE agent #{}".format(n), full_record["steps_episode"])
-    
-    len_full = [len(steps) for steps in episode_steps_full]
-    steps_arr_full = [steps[:min(len_full)] for steps in episode_steps_full]
-    steps_arr_full = np.asarray(steps_arr_full)
-    mean_full = np.mean(steps_arr_full, axis=0)
-    std_full = np.std(steps_arr_full, axis=0)
-    plt.errorbar(np.arange(1, min(len_full)+1), mean_full, std_full, linestyle='None', marker='^')
-    plt.xlabel("Current episode")
-    plt.ylabel("Steps")
-    plt.show()
 
+    ## run experiments using multiprocessing package for parallel computation
+    n_processes = min(N_AGENTS, os.cpu_count())
+    with Pool(processes=n_processes) as pool:
+        results = pool.map(run_exp_parallel, range(N_AGENTS))
+        
+    full_records = [agent[0] for agent in results]
+    inst_records = [agent[1] for agent in results]
+    epis_records = [agent[2] for agent in results]
+    rand_records = [agent[3] for agent in results]
+    run_time = [full_record["runtime"] for full_record in full_records] # runtime for full agents across trials
+    
+    ## Calculate and visualize statistics of steps needed per episode for ##
+    ## agents to reach the goal ##
+    # episode_steps_full = [full_record["steps_episode"] for full_record in full_records]
+    # episode_steps_inst = [inst_record["steps_episode"] for inst_record in inst_records]
+    # episode_steps_epis = [epis_record["steps_episode"] for epis_record in epis_records]
+    # len_full = [len(steps) for steps in episode_steps_full]
+    # steps_arr_full = [steps[:min(len_full)] for steps in episode_steps_full]
+    # steps_arr_full = np.asarray(steps_arr_full)
+    # mean_full = np.mean(steps_arr_full, axis=0)
+    # std_full = np.std(steps_arr_full, axis=0)
+    # plt.errorbar(np.arange(1, min(len_full)+1), mean_full, std_full, linestyle='None', marker='^')
+    # plt.xlabel("Current episode")
+    # plt.ylabel("Steps")
+    # plt.show()
+
+    ## save results for all agents of all types ##
     np.save(os.path.join(path, "full_records.npy"), np.stack(full_records))
     np.save(os.path.join(path, "inst_records.npy"), np.stack(inst_records))
     np.save(os.path.join(path, "epis_records.npy"), np.stack(epis_records))
